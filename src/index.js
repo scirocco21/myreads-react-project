0448d530b0703e2fde1a1c096ebe8e21e052c527
Sebastian <sebgertz@gmail.com>
2018-11-26 17:56:28 -0500

    build: added correct bootstrap files and fixed imports in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import './css/bootstrap.css';
+import './css/bootstrap-theme.css';
 
 ReactDOM.render(<App />, document.getElementById('root'))
