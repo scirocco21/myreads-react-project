0cee7dc92d3edd50fe93b0e29957df1141596dd5
Sebastian <sebgertz@gmail.com>
2018-12-07 12:25:01 -0500

    bug fix: attempt to display thumbnail only if imageLinks property is truthy; refactor: set select value to shelf props only

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -6,9 +6,9 @@ class Book extends Component {
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail}")` }}></div>
           <div className="book-shelf-changer">
-            <select onChange={(e) => this.props.changeShelf(e, this.props.book)} value={this.props.book.shelf || "none"}>
+            <select onChange={(e) => this.props.changeShelf(e, this.props.book)} value={this.props.book.shelf}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
